bcbd1a9842a10470578cf0cb7e61efdd11344d65
gauharnawab <gauharnawab20@gmail.com>
2024-03-08 21:50:41 +0530

    CeilingOfNumber  and floor of a number program

---
+++ binarysearchalgorithm/CeilingOfNumber.java
@@ -0,0 +1,27 @@
+package binarysearchalgorithm;
+
+public class CeilingOfNumber {
+    //Ceiling : Smallest number in the array that is greater or than or equal to the target element
+    static int ceilingNumber(int[] arr , int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if(target < arr[mid]){
+                end = mid-1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = ceilingNumber(arr,target);
+        System.out.println(ans);
+    }
+}
