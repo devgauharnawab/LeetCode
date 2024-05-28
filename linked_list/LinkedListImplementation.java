adff7b6d25f2e746e49b6ecdbfa8f43b798721de
gauharnawab <gauharnawab20@gmail.com>
2024-05-29 04:22:15 +0530

    add method to insert node at the end of the linked list

--- linked_list/LinkedListImplementation.java
+++ linked_list/LinkedListImplementation.java
@@ -27,11 +27,37 @@ public class LinkedListImplementation {
         }
         System.out.println("null");
     }
+
+    //insert element at the end of the linked list
+    public void insertAtEnd(int data){
+        //first, we create a new node with value 6
+        Node newNode = new Node(data);
+        //check if the head of the linked list is null, this means list is empty, and the new node become the head of the linked list
+        if (head == null){
+            head = newNode;
+            return;
+        }else{
+            //we initialize a pointer current to the head of the list
+            Node current = head;
+            while (current.next != null){
+                current = current.next;
+            }
+            //after this loop current points to the last node
+            //insert node at the end
+            current.next = newNode;
+        }
+    }
     public static void main(String[] args){
         LinkedListImplementation ll = new LinkedListImplementation();
+        ll.insertElementAtBeginning(5);
+        ll.insertElementAtBeginning(4);
         ll.insertElementAtBeginning(3);
         ll.insertElementAtBeginning(2);
         ll.insertElementAtBeginning(1);
+        ll.display();
+
+        System.out.println("Insert new Element at the End");
+        ll.insertAtEnd(6);
         ll.display();
     }
 }
